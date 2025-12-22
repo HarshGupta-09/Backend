4a1fe2579941d674251268ee753820b2350e9ca4
Harsh Gupta <harshgupta82003@gmail.com>
2025-12-22 18:23:58 +0530

    added cors middleware

--- 05-MiddleWare and CORS/cors.js
+++ 05-MiddleWare and CORS/cors.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const app = express();
+
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 app.post("/sum", function(req, res) {
     const a = Number(req.query.a);
     const b = Number(req.query.b);
@@ -22,5 +25,5 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(3000 ,()=>{
-    console.log("server Running on Port 3000")
+    console.log("server Running on Port 3000" + " http://localhost:3000" )
 });
